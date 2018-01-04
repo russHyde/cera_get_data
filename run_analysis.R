0419ef7d958946f7a449e9edb08285a7b26880f9
Russell Hyde <russ_hyde@hotmail.com>
2018-01-04 12:18:15 +0000

    drops zip files and datafiles from git

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+
+
 # This script should do the following:
 # Read in the UCI/HAR dataset
 # Merge the training and test data into a single dataset
@@ -9,5 +11,18 @@
 # - this should contain the average of each variable for each activity and
 # subject
 
+# tidy-data should have the following:
+# - each measured variable in a single column
+# - each different observation should be in a different row
+# - one table for each kind of data
+# - when multiple tables are present, there should be a linking column for use
+# in merging
 
+# code-book should contain:
+# - info about the variables in the submitted data (units etc)
+# - info about the summary choices (eg, use of mean or median)
+# - info about the expt study design
+# - Section called 'Study design' containing thorough description of data
+# collection
+# - Section called 'Code book' that describes each var and its units
 
