f7116bcc73ca31f3e4f29abfee0a94cd745f4943
Russell Hyde <russ_hyde@hotmail.com>
2018-01-04 11:53:32 +0000

    explains purpose of the script

---
+++ run_analysis.R
@@ -0,0 +1,13 @@
+# This script should do the following:
+# Read in the UCI/HAR dataset
+# Merge the training and test data into a single dataset
+# Subset the columns of the dataset - keeping the mean and sd for each
+# measurement
+# Rename the activities in the dataset
+# Rename the columns of the dataset
+# Create a second tidy dataset based on the subset/renamed dataset
+# - this should contain the average of each variable for each activity and
+# subject
+
+
+
